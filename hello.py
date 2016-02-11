474fed443acc4b9e4cafe6845f1ca63f8aeb5bbd
MrVoorhees <svoorhees@tburg.k12.ny.us>
2016-02-11 08:29:27 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,11 +2,3 @@
 #Mr. Voorhee$ needs help
 #HelloWorld.py
 print ("Hello World")
-#kms.py
-#Hey there tiger
-#coding is fun
-#yay computer science
-print ("NM, don't eat me. I dont like being dead.")
-#Benedict Cucumberpatch
-#(ノ͡° ͜ʖ ͡°)ノ︵┻┻ IM DONE
-print ("Munchies")
